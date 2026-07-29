ca8c7ebb2b06da075c81965f5784d6a1ab64e36b
nikos.kout9730 <134779828+nick9730@users.noreply.github.com>
2026-07-29 03:41:54 +0300

    Delete Private directory

--- Private/GAS/M_GameplayAbility.cpp
+++
@@ -1,4 +0,0 @@
-
-
-#include "GAS/M_GameplayAbility.h"
-
--- Private/Simple_Inventory/M_InventoryInterface.cpp
+++
@@ -1,6 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "Simple_Inventory/M_InventoryInterface.h"
-
-// Add default functionality here for any IM_InventoryInterface functions that are not pure virtual.
